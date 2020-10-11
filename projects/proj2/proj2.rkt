69f6129710fdbeb8e521d4f5e137f497eb5d6496
Gabriel Wallace <gabe.wallace04@gmail.com>
2020-10-11 15:18:38 -0500

    Complete question 1 and add example racket files

--- projects/proj2/proj2.rkt
+++ projects/proj2/proj2.rkt
@@ -1,3 +1,30 @@
 #lang racket
 
+(define pi 3.1416)
+(define (square x)
+  (* x x))
+(define (cube x)
+  (* x x x))
+  
+(define (my_calc1 x r)
+  (cond
+    ((= x 1) (* pi (square r)))
+    ((= x 2) (*(/ 4 3) pi (cube r)))
+    (else #f)
+    )
+  )
 
+(define (my_calc2 x r)
+  (if (= x 1) (* pi (square r))
+   (if (= x 2) (*(/ 4 3) pi (cube r)) #f))
+  )
+
+
+(my_calc1 1 2)
+(my_calc1 2 2)
+(my_calc1 3 2)
+(my_calc2 1 2)
+(my_calc2 2 2)
+(my_calc2 3 2)
+
+     
